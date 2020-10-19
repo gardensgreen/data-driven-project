5d4a2770dd1f384378a10e8a8cf71e7fe3124511
Daniel TIllero <danieltillero67@gmail.com>
2020-10-19 08:22:59 -0400

    turkey in the oven boom

--- config/database.js
+++ config/database.js
@@ -9,3 +9,30 @@ module.exports = {
         dialect: "postgres",
     },
 };
+
+// {
+//   "development": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_development",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql",
+//     "operatorsAliases": false
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql",
+//     "operatorsAliases": false
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql",
+//     "operatorsAliases": false
+//   }
+// }
