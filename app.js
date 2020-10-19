dd82e9713623894abdeae94d618b1b7f75918ba9
Daniel TIllero <danieltillero67@gmail.com>
2020-10-19 04:55:56 -0400

    add morgan middleware and create bin

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const routes = require("./routes");
 
 const app = express();
 
+app.use(morgan("dev"));
 app.set("view engine", "pug");
 
 app.use(routes);
 
-const port = 8080;
-app.listen(port, () => console.log(`Listening on port: ${port}...`));
+module.exports = app;
