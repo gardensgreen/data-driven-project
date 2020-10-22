438efc12a0d5be0435deae328465d2f326e96842
Jhonathan Ade <ade.jhonathan24@gmail.com>
2020-10-22 19:06:32 -0400

    really close to the end!

--- routes.js
+++ routes.js
@@ -104,4 +104,52 @@ router.post(
     })
 );
 
+router.get('/book/edit/:id(\\d+)', csrfProtection,
+    asyncHandler(async (req, res) => {
+        const bookId = parseInt(req.params.id, 10);
+        const book = await db.Book.findByPk(bookId);
+        res.render('book-edit', {
+            title: 'Edit Book',
+            book,
+            csrfToken: req.csrfToken(),
+        });
+    }));
+
+router.post('/book/edit/:id(\\d+)', csrfProtection, bookValidators,
+    asyncHandler(async (req, res) => {
+        const bookId = parseInt(req.params.id, 10);
+        const bookToUpdate = await db.Book.findByPk(bookId);
+
+        const {
+            title,
+            author,
+            releaseDate,
+            pageCount,
+            publisher,
+        } = req.body;
+
+        const book = {
+            title,
+            author,
+            releaseDate,
+            pageCount,
+            publisher,
+        };
+
+        const validatorErrors = validationResult(req);
+
+        if (validatorErrors.isEmpty()) {
+            await bookToUpdate.update(book);
+            res.redirect('/');
+        } else {
+            const errors = validatorErrors.array().map((error) => error.msg);
+            res.render('book-edit', {
+                title: 'Edit Book',
+                book: { ...book, id: bookId },
+                errors,
+                csrfToken: req.csrfToken(),
+            });
+        }
+    }));
+
 module.exports = router;
